aa28f9eba5e8c040044536ca62c9d02984f3c90a
宣炳刚 <xan.brian@gmail.com>
2018-02-02 13:51:19 +0800

    fix bug

--- src/main/java/com/insight/usercenter/Application.java
+++ src/main/java/com/insight/usercenter/Application.java
@@ -2,17 +2,22 @@ package com.insight.usercenter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 宣炳刚
  * @date 2017/9/15
  * @remark 应用入口程序
  */
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
+    /**
+     * 应用入口方法
+     *
+     * @param args 启动参数
+     */
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
